fb34cb216f41cf0975ee6c13a77966388e8e1460
Hyeokzins <128476961+Hyeokzins@users.noreply.github.com>
2025-06-23 20:21:30 +0900

    Add files via upload

---
+++ install_models.ps1
@@ -0,0 +1,10 @@
+# 모델 목록
+$models = @("llama3.1:8b-instruct-q8_0", "llama3.1", "exone3.5:7.8b", "codellama:instruct","codellama:python","deepseek-r1:7b","daynice/kure-v1")
+
+# 각 모델 설치
+foreach ($model in $models) {
+    Write-Host "Installing $model..."
+    ollama pull $model
+}
+
+Write-Host "All models installed!"
