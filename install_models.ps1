32661e740e2a115864474d7ccbbb2186d22c684c
Hyeokzins <128476961+Hyeokzins@users.noreply.github.com>
2025-09-16 18:30:38 +0900

    Update install_models.ps1

--- install_models.ps1
+++ install_models.ps1
@@ -1,5 +1,5 @@
 # 모델 목록
-$models = @("llama3.1:8b-instruct-q8_0", "llama3.1", "exaone3.5:7.8b", "codellama:instruct","codellama:python","deepseek-r1:7b","daynice/kure-v1")
+$models = @("llama3.1:8b-instruct-q8_0", "llama3.1", "exaone3.5:7.8b", "deepseek-coder-v2:16b","qwen2.5:14b","nomic-embed-text","llava:7b","codellama:instruct","deepseek-r1:7b","daynice/kure-v1")
 
 # 각 모델 설치
 foreach ($model in $models) {
@@ -8,3 +8,4 @@ foreach ($model in $models) {
 }
 
 Write-Host "All models installed!"
+
